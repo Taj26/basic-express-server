8ba9e0ff2ef93887b4ab0f10c93e1c0ffeea9df1
Taj26 <mohammedbabataj02@gmail.com>
2025-08-25 09:12:45 +0000

    put and delet

---
+++ task1/put.js
@@ -0,0 +1,42 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json()) // This line imp for post,put,delete apis
+
+const PORT = 5001;
+
+
+app.get("/global",(req,res)=>{
+  res.send("Hello Students")
+})
+
+app.post("/register",(req,res)=>{
+    let userData = req.body
+    console.log(userData)
+    // res.json(userData)
+    // // or
+    res.json({msg:"user registered successfully"})
+})
+
+app.put("/students/:roll",(req,res)=>{
+  let id = req.params.roll;
+  console.log(id)
+  res.json(id)
+})
+
+app.put("/students/:roll",(req,res)=>{
+  let id = req.params.roll;
+  console.log(id)
+  res.json(id)
+})
+
+app.delete("/students/:roll",(req,res)=>{
+  let id = req.params.roll;
+  console.log(id)
+  res.json(id)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
